8661de31a558723bd07c519ecfd4cf16dda4775a
ashok2811 <ashok281196@gmail.com>
2021-09-27 22:53:51 +0530

    min # of del to make palindrome

--- Learning/DP/print_scs.cpp
+++ Learning/DP/print_scs.cpp
@@ -11,14 +11,14 @@ string lcs(string x, string y, int n, int m){
     {
         for (int j = 1; j < m+1; j++)
         {
-            if(x[i-1]==y[j-1]){
+            if(x[i- 1]==y[j-1]){
                 dp[i][j] = 1 + dp[i-1][j-1];
             }
 
             else{
                 dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j]);
             }
-            
+             
         }
     }
 
